be40dece56a9bc80cad05443972fe22babce52ac
Manornir <manornir200@gmail.com>
2024-03-30 22:44:30 +0300

    add without models

--- algotrading_client/stocks_prediction/main/models.py
+++ algotrading_client/stocks_prediction/main/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-# Create your models here.
-#class Stock(models.Model):
-#    name = models.CharField(max_length=100)
-#    price_tomorrow = models.DecimalField(max_digits=100, decimal_places=3)
-
-#    def __str__(self):
-#        return self.name
 
 class Stock(models.Model):
     name = models.CharField(max_length=100)
